a20a39b4a104670d269ef46f76d963068736d00c
Hades62442 <83868670+Hades62442@users.noreply.github.com>
2021-05-08 23:07:12 +0100

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,36 @@
+import random
+
+def main():
+    global reps
+    amountDied = 0
+    days = 100
+    reps = 5000
+    for i in range(reps):
+        j = 0
+        died = False
+        while not died and j < days:
+            roll = random.randint(1, 100)
+            #print(roll)
+            if roll == 79:
+                amountDied += 1
+                died = True
+            j += 1
+    
+    #print(f"Amount of deaths: {amountDied}")
+    #print(f"Percentage of dying: {amountDied/reps*100}%")
+
+    return amountDied
+
+
+vals = []
+for i in range(10):
+    amountDied = main()
+    print(f"{amountDied/reps*100}%")
+    vals.append(amountDied/reps*100)
+
+total = 0
+for i in range(len(vals)):
+    total += vals[i]
+
+avg = total/len(vals)
+print(f"Average percentage of getting a heart attack: {avg}%")
